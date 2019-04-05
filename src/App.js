54c82e28e97c1ca0bf83139e5e85326ddd34f78a
Aydin Akan <aakan@smartover.net>
2019-04-05 14:51:00 +0300

    app converted to func component

--- src/App.js
+++ src/App.js
@@ -1,35 +1,29 @@
-import React, { Component, Suspense } from 'react';
+import React, { Suspense } from 'react';
 import { Player } from './video-player';
 import './i18n/i18n';
 
-class App extends Component {
-  playerReady = () => {
-    // console.log('player is ready');
+const App = () => {
+  const playerOptions = {
+    poster: '//d2zihajmogu5jn.cloudfront.net/elephantsdream/poster.png',
+    sources: [
+      {
+        src: '//d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4',
+        type: 'video/mp4'
+      }
+    ],
+    fluid: true,
+    title: 'Video Title',
+    description: 'Video description',
+    language: 'tr'
   };
 
-  render() {
-    const playerOptions = {
-      poster: '//d2zihajmogu5jn.cloudfront.net/elephantsdream/poster.png',
-      sources: [
-        {
-          src: '//d2zihajmogu5jn.cloudfront.net/elephantsdream/ed_hd.mp4',
-          type: 'video/mp4'
-        }
-      ],
-      fluid: true,
-      title: 'Video Title',
-      description: 'Video description',
-      language: 'tr'
-    };
-
-    return (
-      <div>
-        <Suspense fallback={<></>}>
-          <Player {...playerOptions} onReady={() => this.playerReady()} />
-        </Suspense>
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Suspense fallback={<></>}>
+        <Player {...playerOptions} />
+      </Suspense>
+    </div>
+  );
+};
 
 export default App;
