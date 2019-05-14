9242e4100156fb2055f82db2ba92aa7435e0a6b0
Aydin Akan <aakan@smartover.net>
2019-05-14 09:35:34 +0300

    hotspot states

--- src/store/redux/hotspot/hotspotOperations.js
+++ src/store/redux/hotspot/hotspotOperations.js
@@ -13,16 +13,24 @@ export const onAdd = (newHotspots) => {
   };
 };
 
+export const setActiveHotspotIds = (ids) => {
+  return (dispatch) => {
+    dispatch(actions.setActiveHotspotIds(ids));
+  };
+};
+
 const mapStateToProps = (state) => {
   return {
-    hotspots: state.hotspots
+    hotspots: state.hotspots.hotspots,
+    activeHotspotIds: state.hotspots.activeHotspotIds
   };
 };
 
 const mapDispatchToProps = (dispatch) => {
   return {
     onFieldUpdate: (newStatus) => dispatch(onFieldUpdate(newStatus)),
-    onAdd: (newHotspots) => dispatch(onAdd(newHotspots))
+    onAdd: (newHotspots) => dispatch(onAdd(newHotspots)),
+    setActiveHotspotIds: (ids) => dispatch(setActiveHotspotIds(ids))
   };
 };
 
