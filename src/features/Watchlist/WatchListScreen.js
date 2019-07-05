33262a73e5c75a04b5923912e027d582f1a7531a
asuha isik <aisik@smartover.net>
2019-07-05 11:55:57 +0300

    EmptyWatchList component is done

--- src/features/Watchlist/WatchListScreen.js
+++ src/features/Watchlist/WatchListScreen.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import EmptyList from '../../components/EmptyList/EmptyList';
+import EmptyWatchList from './EmptyWatchList';
 
 const WatchListScreen = () => {
-  return <EmptyList />;
+  // TODO: The other screen will be came
+  return <EmptyWatchList />;
 };
 
 export default WatchListScreen;
