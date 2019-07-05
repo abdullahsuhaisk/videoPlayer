db7e32b76b75b1de642da04452528f5276669328
asuha isik <aisik@smartover.net>
2019-07-05 10:34:56 +0300

    Emptylist base component is done

--- src/components/EmptyList/EmptyList.js
+++ src/components/EmptyList/EmptyList.js
@@ -4,7 +4,7 @@ import PropTypes from 'prop-types';
 import { loadWebFontsFromStyles } from '../../utils/parseStyles';
 import { emptylistStyle, Wrapper } from './EmptyList.style';
 
-const EmptyList = ({ styles, image, content }) => {
+const EmptyList = ({ styles, imageUrl, content }) => {
   useEffect(() => {
     loadWebFontsFromStyles(emptylistStyle);
     loadWebFontsFromStyles(styles);
@@ -15,12 +15,11 @@ const EmptyList = ({ styles, image, content }) => {
       <div className="vb--component--emptyList-container">
         <div
           className="vb--component--emptyList-image"
-          style={{
-            background:
-              'url(/images/emptyWhatchList.png) center center no-repeat'
-          }}
+          style={{ backgroundImage: `url(${imageUrl})` }}
         />
-        <div className=".vb--component--emptyList-content">{content}</div>
+        <div className=".vb--component--emptyList-content">
+          <span className="bold">{content}</span>
+        </div>
       </div>
     </Wrapper>
   );
@@ -29,13 +28,13 @@ const EmptyList = ({ styles, image, content }) => {
 EmptyList.propTypes = {
   styles: PropTypes.object,
   content: PropTypes.string,
-  image: PropTypes.string
+  imageUrl: PropTypes.string
 };
 
 EmptyList.defaultProps = {
   styles: {},
   content: 'No something in the list',
-  image: 'url(/images/emptyWhatchList.png) center center no-repeat'
+  imageUrl: '/images/emptyWhatchList.png'
 };
 
 export default EmptyList;
---
+++ src/features/Watchlist/WatchListScreen.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import EmptyList from '../../components/EmptyList/EmptyList';
+
+const WatchListScreen = () => {
+  return <EmptyList />;
+};
+
+export default WatchListScreen;
