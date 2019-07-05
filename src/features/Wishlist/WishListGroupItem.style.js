bfed05c68c6da28737fa956882ef3c03c1052a85
asuha isik <aisik@smartover.net>
2019-07-05 15:00:59 +0300

    wishlist group item component is okey

---
+++ src/features/Wishlist/WishListGroupItem.style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components';
+
+export const wishlistgroupStyle = {
+  width: '100px',
+  height: '120px',
+  borderRadius: '8px',
+  boxShadow: '0 2px 5px 0 rgba(0, 0, 0, 0.16)',
+  backgroundColor: '#ffffff',
+  '.vb--wishlist--group--item--container': {
+    width: '100%',
+    height: '100%',
+    padding: '1px'
+  },
+  '.vb--wishlist--group--item--image': {
+    background: 'url(/images/emptyFavorite.svg) center center no-repeat',
+    display: 'inline-block',
+    width: '99px',
+    height: '100px',
+    objectFit: 'contain',
+    padding: '1px',
+    backgroundSize: 'contain'
+  },
+  '.vb--wishlist--group--item--price': {
+    display: 'flex',
+    justifyContent: 'center',
+    paddingTop: '2px'
+  },
+  span: {
+    color: '#0e273b',
+    textAlign: 'center',
+    fontFamily: 'Source Sans Pro',
+    marginBottom: '-10px',
+    fontSize: '12px'
+  }
+};
+
+export const Wrapper = styled.div((props) => ({
+  ...wishlistgroupStyle,
+  ...props.styles
+}));
--- src/features/Wishlist/WishlistScreen.js
+++ src/features/Wishlist/WishlistScreen.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import EmptyWishList from './EmptyWishList';
+import WishListGroupItem from './WishListGroupItem';
 
 const WishlistScreen = () => {
   return (
-    <div style={{ width: '100%', height: '500px' }}>
+    <div style={{ width: '100%', height: '200px' }}>
       <EmptyWishList />
+      <WishListGroupItem />
     </div>
   );
 };
