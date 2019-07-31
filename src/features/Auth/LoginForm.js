f67df3c6baeb29d1dd4a448ae035f579f646bade
asuha isik <aisik@smartover.net>
2019-07-31 11:45:20 +0300

    login form is done

--- src/features/Auth/LoginForm.js
+++ src/features/Auth/LoginForm.js
@@ -42,102 +42,112 @@ const LoginForm = ({ styles }) => {
     <Mutation mutation={LOGIN}>
       {(login, { data, client }) => {
         return (
-          <div className="login" style={{ pointerEvents: 'auto' }}>
-            <form
-              onSubmit={(e) => {
-                e.preventDefault();
-                login({
-                  variables: {
-                    email: emailRef.current.value,
-                    password: passwordRef.current.value
-                  }
-                });
-              }}>
+          <React.Fragment>
+            <div className="login">
               <figure className="login--imgWrapper">
                 <img className="login--img" src="/images/login_image.svg" />
               </figure>
-              <div className="login--inputWrapper">
-                <span className="login--label">Mobile Number or Email</span>
-                <input
-                  type="email"
-                  className="login--input"
-                  placeholder="Email"
-                  ref={emailRef}
-                />
-              </div>
-              <div className="login--inputWrapper">
-                <span className="login--label">Password</span>
-                <input
-                  type="password"
-                  className="login--input"
-                  placeholder="Password"
-                  ref={passwordRef}
-                />
-              </div>
-              <div className="login--resetWrapper">
-                <div className="login--staySigned">
-                  <input type="checkbox" id="stay_signed" />{' '}
-                  <label className="login--staySigned--label" for="stay_signed">
-                    Stay signed in
-                  </label>
+              <form
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  login({
+                    variables: {
+                      email: emailRef.current.value,
+                      password: passwordRef.current.value
+                    }
+                  });
+                }}>
+                <div className="login--emailWrapper">
+                  <span className="login--label">Mobile Number or Email</span>
+                  <input
+                    type="email"
+                    className="login--input"
+                    placeholder="Email"
+                    ref={emailRef}
+                  />
                 </div>
-                <div className="login--forgotPassword">
-                  <div
-                    className="login--forgotPassword--a"
-                    role="button"
-                    tabIndex="0"
+                <div className="login--passwordWrapper">
+                  <span className="login--label">Password</span>
+                  <input
+                    type="password"
+                    className="login--input"
+                    placeholder="Password"
+                    ref={passwordRef}
+                  />
+                </div>
+                <div className="login--resetWrapper">
+                  <div className="login--staySigned">
+                    <input
+                      type="checkbox"
+                      id="stay_signed"
+                      className="login--staySigned-checkbox"
+                    />{' '}
+                    <label
+                      className="login--staySigned--label"
+                      HtmlFor="stay_signed">
+                      Stay signed in
+                    </label>
+                  </div>
+                  <div className="login--forgotPassword">
+                    <div
+                      className="login--forgotPassword--a"
+                      role="button"
+                      tabIndex="0"
+                      onClick={() =>
+                        client.writeData({
+                          data: {
+                            isLoginFormShowing: false,
+                            isForgotPasswordFormShowing: true
+                          }
+                        })
+                      }>
+                      Forgot your password?
+                    </div>
+                  </div>
+                </div>
+                <div className="login--buttonWrapper">
+                  <button className="login--button">Login</button>
+                </div>
+                <div className="loginWithWrapper">
+                  <p className="login--loginWith">or Login with</p>
+                  <div className="login--withSocialMedia">
+                    <div
+                      href="#"
+                      className="login--withFacebook"
+                      role="button"
+                      tabIndex="-1"
+                      onClick={() =>
+                        client.mutate({ mutation: LOGIN_WITH_GOOGLE })
+                      }></div>
+                    <div
+                      href="#"
+                      className="login--withGoogle"
+                      role="button"
+                      tabIndex="-1"
+                      onClick={() =>
+                        client.mutate({ mutation: LOGIN_WITH_FACEBOOK })
+                      }></div>
+                  </div>
+                </div>
+                <hr className="login--hr" />
+                <p className="login-haveAccount">Don’t have an account? </p>
+                <div className="login--signupButtonWrapper">
+                  <button
+                    className="login--signupButton"
                     onClick={() =>
                       client.writeData({
                         data: {
                           isLoginFormShowing: false,
-                          isForgotPasswordFormShowing: true
+                          isRegisterFormShowing: true
                         }
                       })
                     }>
-                    Forgot your password?
-                  </div>
+                    Sign up
+                  </button>
                 </div>
-              </div>
-              <div className="login--buttonWrapper">
-                <button className="login--button">Login</button>
-              </div>
-              <div className="loginWithWrapper">
-                <p className="login--loginWith-p">or Login with</p>
-                <div className="login--withSocialMedia">
-                  <div
-                    href="#"
-                    className="login--withFacebook"
-                    role="button"
-                    tabIndex="-1"
-                    onClick={() =>
-                      client.mutate({ mutation: LOGIN_WITH_GOOGLE })
-                    }></div>
-                  <div
-                    href="#"
-                    className="login--withGoogle"
-                    role="button"
-                    tabIndex="-1"
-                    onClick={() =>
-                      client.mutate({ mutation: LOGIN_WITH_FACEBOOK })
-                    }></div>
-                </div>
-              </div>
-              <hr className="login--hr" />
-              <p className="login-haveAccount">Don’t have an account? </p>
-              <button
-                className="login--signupButton"
-                onClick={() =>
-                  client.writeData({
-                    data: {
-                      isLoginFormShowing: false,
-                      isRegisterFormShowing: true
-                    }
-                  })
-                }>
-                Sign up
-              </button>
-            </form>
-          </div>
+              </form>
+            </div>
+          </React.Fragment>
         );
       }}
     </Mutation>
