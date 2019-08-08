9814753cbf05d13bcc8178a6aaa22b8bd54578fd
asuha isik <aisik@smartover.net>
2019-08-08 17:56:49 +0300

    Countinue at home

--- src/components/Base/BaseQueries.js
+++ src/components/Base/BaseQueries.js
@@ -21,3 +21,52 @@ export const GET_PRODUCT_ID = gql`
     productId @client
   }
 `;
+
+export const LIKE_PRODLINK = gql`
+  mutation LikeProdLink($prodLinkId: Int!) {
+    likeProdLink(prodLinkId: $prodLinkId) {
+      id
+      name
+      header
+      description
+    }
+  }
+`;
+
+export const UNLIKE_PRODLINK = gql`
+  mutation UnLikeProdLink($prodLinkId: Int!) {
+    unlikeProdLink(prodLinkId: $prodLinkId) {
+      id
+      name
+      header
+      description
+    }
+  }
+`;
+
+// const likeFragment = '';
+
+export const ADD_PRODLINK_TO_FAVORITE = gql`
+  mutation AddProdLinkToFavorite($prodLinkId: Int!) {
+    addProdLinkToFavorite(prodLinkId: $prodLinkId) {
+      id
+      name
+      header
+      numberOfViews
+      numberOfLikes
+      numberOfShares
+    }
+  }
+`;
+export const DELETE_PRODLINK_TO_FAVORITE = gql`
+  mutation deleteProdLinkFromFavorite($prodLinkId: Int!) {
+    deleteProdLinkFromFavorite(prodLinkId: $prodLinkId) {
+      id
+      name
+      header
+      numberOfViews
+      numberOfLikes
+      numberOfShares
+    }
+  }
+`;
