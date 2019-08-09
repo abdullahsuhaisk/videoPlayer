60d0093bc87a16d0f655b2e3ec715a27eea0b2b1
asuha isik <aisik@smartover.net>
2019-08-09 14:21:46 +0300

    WachListbutton is okey

--- src/components/Template/Tab/WatchListButton.js
+++ src/components/Template/Tab/WatchListButton.js
@@ -1,3 +1,4 @@
+/* eslint-disable jsx-a11y/no-static-element-interactions */
 /* eslint-disable jsx-a11y/click-events-have-key-events */
 import React from 'react';
 import { Query, Mutation } from 'react-apollo';
@@ -8,16 +9,29 @@ import {
 } from '../../../features/Watchlist/WatchListQueries';
 import { PRODLINK_ID } from '../../../common/GrapqlConstant';
 
+function desider(List, item) {
+  const listArray = [];
+  List && List.map((i) => listArray.push(i.id));
+  console.log(listArray && listArray.includes(item));
+  return listArray && listArray.includes(item);
+}
+
 const WatchListButton = () => {
+  const [watchlist, setWatchlist] = React.useState(null);
+  const [watchListButtonManager, setWatchListButtonManager] = React.useState(
+    false
+  );
+  React.useEffect(() => {
+    setWatchListButtonManager(desider(watchlist, PRODLINK_ID));
+  }, [watchlist]);
+
   return (
     <Query query={GET_CONSUMER_WATCH_LISTID}>
       {({ data: { consumer }, loading, error }) => {
         if (loading || error) return null;
-        const watchlist = consumer ? consumer.watchList : null;
-        console.log(watchlist);
-        const watchListButtonManager =
-          watchlist && watchlist.filter((item) => item === PRODLINK_ID);
-        return watchListButtonManager.length > 0 ? (
+        setWatchlist(consumer ? consumer.watchList : null);
+        //  watchlist && watchlist.filter((item) => item === PRODLINK_ID);
+        return watchListButtonManager === true ? (
           <DeleteWatchList PRODLINK_ID={PRODLINK_ID} />
         ) : (
           <AddWatchList PRODLINK_ID={PRODLINK_ID} />
@@ -36,7 +50,6 @@ const AddWatchList = () => {
       mutation={ADD_WATCH_LIST}
       variables={{ prodLinkId: PRODLINK_ID }}
       refetchQueries={() => {
-        console.log('refetchQueries');
         return [
           {
             query: GET_CONSUMER_WATCH_LISTID
@@ -53,7 +66,7 @@ const AddWatchList = () => {
               addProdLinkToWatchList();
             }}>
             {/* add 'loved' class name beside 'watchlist--heartIcon' class to display red heart */}
-            <i className="stats--content--starIcon loved"></i> 40
+            <i className="stats--content--starIcon  "></i> 40
           </div>
         );
       }}
@@ -68,7 +81,6 @@ const DeleteWatchList = () => {
       mutation={DELETE_WATCHED_LIST}
       variables={{ prodLinkId: PRODLINK_ID }}
       refetchQueries={() => {
-        console.log('refetchQueries');
         return [
           {
             query: GET_CONSUMER_WATCH_LISTID
@@ -84,8 +96,7 @@ const DeleteWatchList = () => {
             onClick={() => {
               deleteProdLinkFromWatchList();
             }}>
-            {/* add 'loved' class name beside 'watchlist--heartIcon' class to display red heart */}
-            <i className="stats--content--starIcon loved"></i> 40
+            <i className="stats--content--starIcon favorites"></i> 40
           </div>
         );
       }}
