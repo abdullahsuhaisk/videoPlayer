32edbeffb4aba12a634bcf09fb082359728aa943
asuha isik <aisik@smartover.net>
2019-08-27 14:08:53 +0300

    App content Loader is done

--- src/components/ContentLoader/MainLoader.js
+++ src/components/ContentLoader/MainLoader.js
@@ -4,7 +4,14 @@ import ContentLoader from 'react-content-loader';
 const MainLoader = () => {
   return (
     <div className="MainContentLoader">
-      <ContentLoader type="facebook" />
+      <ContentLoader
+        height={600}
+        width={800}
+        speed={2}
+        primaryColor="#cadbe2"
+        secondaryColor="#a9bac1">
+        <rect x="200" y="150" rx="4" ry="4" width="400" height="300" />
+      </ContentLoader>
     </div>
   );
 };
