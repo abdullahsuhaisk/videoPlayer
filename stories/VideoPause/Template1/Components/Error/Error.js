df06bc75fcdfdc3fb0d0a72effcbb4d4016257dd
asuha isik <aisik@smartover.net>
2019-09-10 14:39:54 +0300

    Error component is refactored

--- stories/VideoPause/Template1/Components/Error/Error.js
+++ stories/VideoPause/Template1/Components/Error/Error.js
@@ -6,14 +6,25 @@ const Error = () => {
       {/* <div class="stars"></div>
       <div class="twinkling"></div>
       <div class="clouds"></div> */}
-      <div class="error_bg"></div>
+      <div className="error_bg"></div>
       <div className="Error--container">
         <figure className="Error--logo">
           <img src="/images/logo-white.svg" className="Error--logo--img" alt="Vibuy"/>
         </figure>
         <p className="Error--404">404</p>
-        <p className="Error--description">Opss, something went wrong!</p>
-        <button className="Error--returnBtn">Return to home</button>
+        {content.map((item, key) => (
+          <p className="Error--description" key={key}>
+            {item.code === 'ERR004' ? 'There is no ProdLink' : item.message}
+          </p>
+        ))}
+        <button
+          className="Error--returnBtn"
+          onClick={async () => {
+            goVideo();
+          }}>
+          Return to home
+        </button>
+
       </div>
       <div className="Error--stars">
         <div id='stars'></div>
