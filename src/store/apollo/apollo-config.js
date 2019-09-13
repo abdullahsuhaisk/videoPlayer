62c0672ba0399d069a9f7fe3869416e905c0fdee
asuha isik <aisik@smartover.net>
2019-09-13 10:47:40 +0300

    Hoc Query is added

--- src/store/apollo/apollo-config.js
+++ src/store/apollo/apollo-config.js
@@ -66,7 +66,7 @@ const errorLink = onError(
             });
             // retry the request, returning the new observable
             console.log(operation);
-            window.location.reload();
+
             return forward(operation);
           default:
             console.log(err.code);
@@ -75,7 +75,10 @@ const errorLink = onError(
     }
     if (networkError && networkError.statusCode === 401) {
       // eslint-disable-next-line
-      window.location.reload();
+      console.log(networkError);
+      getNewToken().then(() => {
+        window.location.reload();
+      });
     } else if (networkError) console.log(`[Network error]: ${networkError}`);
   }
 );
