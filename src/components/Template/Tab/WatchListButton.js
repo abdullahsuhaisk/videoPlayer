7538604dea0ea8316e79bcfe86ab39338ca1f602
asuha isik <aisik@smartover.net>
2019-09-13 18:22:03 +0300

    for weekend

--- src/components/Template/Tab/WatchListButton.js
+++ src/components/Template/Tab/WatchListButton.js
@@ -8,21 +8,14 @@ import {
   ADD_WATCH_LIST,
   DELETE_WATCHED_LIST
 } from '../../../features/Watchlist/WatchListQueries';
-import { getProdLinkIdApollo } from '../../../hooks/ProdLinkHook';
+import { getProdLinkUniqueId } from '../../../hooks/ProdLinkHook';
 import FavoriteLottie from '../../Lottie/Favorites/FavoriteLottie';
 
-export const GET_CONSUMER = gql`
-  query getConsumerWhatchList {
-    consumer @client {
-      id
-    }
-  }
-`;
-
 const GET_NUMBER_OF_VIDEOTHINGS = gql`
-  query prodLinkIsLikedByCustomer($prodLinkId: Int!) {
-    prodLink(prodLinkId: $prodLinkId) {
+  query prodLinkIsLikedByCustomer($prodLinkUniqueId: String) {
+    prodLink(prodLinkUniqueId: $prodLinkUniqueId) {
       id
+      uniqueId
       # isLiked
       numberOfLikes
       numberOfViews
@@ -33,35 +26,45 @@ const GET_NUMBER_OF_VIDEOTHINGS = gql`
 
 function desider(List, item) {
   const listArray = [];
-  List && List.map((i) => listArray.push(i.id));
+  List && List.map((i) => listArray.push(i.prodLinkUniqueId));
   return listArray && listArray.includes(item);
 }
 
-const WatchListButton = ({ client }) => {
-  const PRODLINK_ID = parseInt(getProdLinkIdApollo(client), 10);
+const WatchListButton = (props) => {
+  const { client } = props;
+  const prodLinkUniqueId = getProdLinkUniqueId();
+
   const [watchlist, setWatchlist] = React.useState(null);
+  const [PRODLINK_ID, SETPRODLINK_ID] = React.useState(null);
   const [watchListButtonManager, setWatchListButtonManager] = React.useState(
     false
   );
   const [watchListCounter, setWatchListCounter] = React.useState(null);
+
+  console.log(watchlist);
+  console.log(PRODLINK_ID);
+  console.log(watchListButtonManager);
+  console.log(watchListCounter);
+
   React.useEffect(() => {
-    setWatchListButtonManager(desider(watchlist, PRODLINK_ID));
+    setWatchListButtonManager(desider(watchlist, prodLinkUniqueId));
     // setAnimate(!desider(watchlist, PRODLINK_ID));
   }, [watchlist]);
-
+  console.log(watchListButtonManager);
   React.useEffect(() => {
     client
       .query({
         query: GET_NUMBER_OF_VIDEOTHINGS,
-        variables: { prodLinkId: PRODLINK_ID }
+        variables: { prodLinkUniqueId }
       })
       .then(({ data }) => {
         setWatchListCounter(data.prodLink.numberOfViews);
+        SETPRODLINK_ID(data.prodLink.id);
       });
   }, []);
 
   return (
-    <Query query={GET_CONSUMER_WATCH_LISTID}>
+    <Query query={GET_CONSUMER_WATCH_LISTID} fetchPolicy="network-only">
       {({ data, loading, error, refetch }) => {
         if (loading) return <FavoriteLottie animate={false} />;
         if (error) return null;
@@ -102,7 +105,7 @@ const AddToWatchlist = ({
   return (
     <Mutation
       mutation={ADD_WATCH_LIST}
-      variables={{ prodLinkId: PRODLINK_ID }}
+      variables={{ prodLinkId: parseInt(PRODLINK_ID, 10) }}
       update={() => refetch()}>
       {(updateProdLinkFromWatchList, { data, error, loading }) => {
         if (loading || error) return null;
@@ -133,7 +136,7 @@ const DeleteFromWatchlist = ({
   return (
     <Mutation
       mutation={DELETE_WATCHED_LIST}
-      variables={{ prodLinkId: PRODLINK_ID }}
+      variables={{ prodLinkId: parseInt(PRODLINK_ID, 10) }}
       update={() => refetch()}>
       {(updateProdLinkFromWatchList, { data, error, loading }) => {
         if (loading || error) return null;
--- src/features/Watchlist/WatchListQueries.js
+++ src/features/Watchlist/WatchListQueries.js
@@ -4,6 +4,7 @@ export const ADD_WATCH_LIST = gql`
   mutation AddWachtListToVideo($prodLinkId: Int!) {
     addProdLinkToWatchList(prodLinkId: $prodLinkId) {
       id
+      uniqueId
       name
       image {
         id
@@ -15,6 +16,7 @@ export const DELETE_WATCHED_LIST = gql`
   mutation deleteProdLinkFromWatchList($prodLinkId: Int!) {
     deleteProdLinkFromWatchList(prodLinkId: $prodLinkId) {
       id
+      uniqueId
       name
       image {
         id
