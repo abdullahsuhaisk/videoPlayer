7b7ca2ab4bd8912eac459071b1c2da56a17cb5d7
usamacjs <umahmood@smartover.net>
2019-09-30 17:22:51 +0300

    some chnages in shopping cart

--- src/features/ShoppingCart/ShoppingCartWithoutLoginContainer.js
+++ src/features/ShoppingCart/ShoppingCartWithoutLoginContainer.js
@@ -2,18 +2,25 @@ import React, { useState } from 'react';
 import ShoppingCartEmpty from '../Product/ProductDetails/Components/Template3/ShoppingCartEmpty';
 import ShoppingCartCheckout from '../Product/ProductDetails/Components/Template3/ShoppingCartCheckout';
 import ShoppingCartItem from '../Product/ProductDetails/Components/Template3/ShoppingCartItem';
+import ScreenChoserQuery from '../../components/HOCS/Grapqhl/ScreenChoserQuery';
 
 // We need a component it like modal
 // It can call cart empty or shopping cart
 // For now ıt just render Empty card for develop to css
 
-const ShoppingCartWithoutLoginContainer = () => {
-  const [data, setData] = useState({ color: 0, size: 0, quality: 1 });
+const ShoppingCartWithoutLoginContainer = ({ client }) => {
+  const [quantity, setQuantity] = useState({ color: 0, size: 0, quality: 1 });
 
   return (
     <div className="shoppingcart--container">
       <div className="shoppingcart--title">Shopping Cart</div>
-      <i className="modal--close"></i>
+      <i
+        className="modal--close"
+        onClick={() =>
+          client.writeData({
+            data: { isShoppingCartShowing: false }
+          })
+        }></i>
       <div className="productdetail--seperator" />
 
       {/* If shopping cart is empty, render ShoppingCArtEmpty */}
@@ -21,11 +28,8 @@ const ShoppingCartWithoutLoginContainer = () => {
 
       {/* If Shopping Cart Is NOT empty , render both ShoppingCartItems and ShoppingCartCheckout	 */}
       <div className="shoppingcart-items-container">
-        <ShoppingCartItem data={data} setData={setData} />
-        <ShoppingCartItem data={data} setData={setData} />
-        <ShoppingCartItem data={data} setData={setData} />
-        <ShoppingCartItem data={data} setData={setData} />
-        <ShoppingCartItem data={data} setData={setData} />
+        <ShoppingCartItem data={quantity} setData={setQuantity} />
+        <ShoppingCartItem data={quantity} setData={setQuantity} />
       </div>
 
       <ShoppingCartCheckout />
@@ -33,4 +37,4 @@ const ShoppingCartWithoutLoginContainer = () => {
   );
 };
 
-export default ShoppingCartWithoutLoginContainer;
+export default ScreenChoserQuery(ShoppingCartWithoutLoginContainer);
