1832f93a67713bd5a7072c94567d80e676312c45
usamacjs <umahmood@smartover.net>
2019-11-05 14:35:24 +0300

    fixed replay screen issue

--- src/components/ControlBarWrapper/ControlBar/VideoProgressBar/VideoProgressBar.js
+++ src/components/ControlBarWrapper/ControlBar/VideoProgressBar/VideoProgressBar.js
@@ -4,16 +4,19 @@ import { withApollo } from 'react-apollo';
 import HotspotPointsScreen from './HotspotPoints/HotspotPointsScreen';
 
 const VideoProgressBar = ({ videoPlayer, client }) => {
+  console.log(videoPlayer && videoPlayer.currentTime().toFixed(9));
+  console.log(videoPlayer && videoPlayer.duration().toFixed(9) - 0.35);
   if (
     videoPlayer &&
-    videoPlayer.currentTime().toFixed(1) >
-      videoPlayer.duration().toFixed(1) - 0.25
+    videoPlayer.currentTime().toFixed(9) >=
+      videoPlayer.duration().toFixed(9) - 0.35
   ) {
     client.writeData({
       data: {
         player: {
           __typename: 'Player',
-          playingState: 'READY'
+          playingState: 'READY',
+          isReady: false
         },
         isLoginFormShowing: false,
         isProfileOpen: false
