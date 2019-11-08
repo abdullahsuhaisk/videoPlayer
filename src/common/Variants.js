96d15a3ddbc76bfea3ed5b4eb487a68ccf721596
asuha isik <aisik@smartover.net>
2019-11-08 10:54:51 +0300

    Mixed config is solved

--- src/common/Variants.js
+++ src/common/Variants.js
@@ -1,6 +1,6 @@
 export const colors = [
   'https://us.123rf.com/450wm/cgdeaw/cgdeaw1905/cgdeaw190500057/122252927-blue-flat-for-background-pastel-blue-color-light-blue-plain-colors-top-view.jpg?ver=6',
-  'http://suriwong.com.my/wp-content/uploads/2017/04/0001-M-Florida.jpg'
+  'https://suriwong.com.my/wp-content/uploads/2017/04/0001-M-Florida.jpg'
 ];
 
 export const sizes = ['XS', 'S', 'M', 'L', 'XL'];
@@ -13,7 +13,7 @@ export const colorsWithName = [
   },
   {
     color:
-      'http://suriwong.com.my/wp-content/uploads/2017/04/0001-M-Florida.jpg',
+      'https://suriwong.com.my/wp-content/uploads/2017/04/0001-M-Florida.jpg',
     name: 'kırmızı'
   }
 ];
