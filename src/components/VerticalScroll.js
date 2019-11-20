894b54e2f0541deb223ddd34ebf5025f4a52933d
usamacjs <umahmood@smartover.net>
2019-11-20 17:27:35 +0300

    scrollbar issue handled for edge browser

--- src/components/VerticalScroll.js
+++ src/components/VerticalScroll.js
@@ -1,7 +1,11 @@
 import React from 'react';
 
 const VerticalScroll = ({ children }) => {
-  return <div className="scroll">{children}</div>;
+  return (
+    <div className="scroll" style={{ overflow: 'hidden' }}>
+      {children}
+    </div>
+  );
 };
 
 export default VerticalScroll;
